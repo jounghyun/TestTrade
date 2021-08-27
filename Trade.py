e7b4c2e0d3b6531c670754d9312adfbb3eb5e9b4
jounghyun <jounghyun@gmail.com>
2021-08-27 21:09:55 +0900

    Update Trade.py
    적극적 구매

--- Trade.py
+++ Trade.py
@@ -36,35 +36,51 @@ while True:
 
         print ('ash(h-2) : {0}, ash(h-1) : {1} , ash_cur : {2}, nor(h-2) : {3}, nor(h-1) : {4}, nor_cur : {5}'.format(df.iloc[97,8], df.iloc[98,8], df.iloc[99,8], df.iloc[97,9], df.iloc[98,9], df.iloc[99,9]))
 
-        order = upbit.get_order("KRW-BTC", state="done")
-        FinalTradeTime = order[0]['created_at']
-
-        TradeFinal = datetime.datetime.strptime(FinalTradeTime, "%Y-%m-%dT%H:%M:%S%z")
-        DateNaive = TradeFinal.replace(tzinfo=None)
-
-        now = datetime.datetime.now()
-        print (DateNaive)
-        print (now)
-
-        dist = now-DateNaive
-        
         btc = get_balance("BTC")
         krw = get_balance("KRW")
 
-        if dist.seconds > 3800 :
-            if df.iloc[97,8] > 0 and df.iloc[98,8] > 0 and df.iloc[98,9] > 0:
-                print("balance : {0} won".format(krw))
-                if krw > 5000:
-                    print("buy : {0}".format(krw*0.9995))
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995) 
-            elif df.iloc[98,8] < 0 and df.iloc[98,9] < 0:            
-                print("current : {0} btc".format(btc))
-                if btc > 0.00008:
-                    print("sell : {0}".format(btc*0.9995))
-                    upbit.sell_market_order("KRW-BTC", btc*0.9995)
-            else:
-                print("hold btc : {0}".format(btc))
-                print("hold won : {0}".format(krw))
+        if df.iloc[97,8] < 0 and df.iloc[98,8] < 0:
+            print("current : {0} btc".format(btc))
+            if btc > 0.00008:
+                print("sell : {0}".format(btc*0.9995))
+                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+        else:
+            print("balance : {0} won".format(krw))
+            if krw > 5000:
+                print("buy : {0}".format(krw*0.9995))
+                upbit.buy_market_order("KRW-BTC", krw*0.9995) 
+
+        # order = upbit.get_order("KRW-BTC", state="done")
+        # FinalTradeTime = order[0]['created_at']
+
+        # TradeFinal = datetime.datetime.strptime(FinalTradeTime, "%Y-%m-%dT%H:%M:%S%z")
+        # DateNaive = TradeFinal.replace(tzinfo=None)
+
+        # now = datetime.datetime.now()
+        # print (DateNaive)
+        # print (now)
+
+        # dist = now-DateNaive
+
+        # print (dist)
+
+        # btc = get_balance("BTC")
+        # krw = get_balance("KRW")
+
+        # if dist.seconds > 3800 :
+        #     if df.iloc[97,8] > 0 and df.iloc[98,8] > 0 and df.iloc[98,9] > 0:
+        #         print("balance : {0} won".format(krw))
+        #         if krw > 5000:
+        #             print("buy : {0}".format(krw*0.9995))
+        #             #upbit.buy_market_order("KRW-BTC", krw*0.9995) 
+        #     elif df.iloc[98,8] < 0 and df.iloc[98,9] < 0:            
+        #         print("current : {0} btc".format(btc))
+        #         if btc > 0.00008:
+        #             print("sell : {0}".format(btc*0.9995))
+        #             #upbit.sell_market_order("KRW-BTC", btc*0.9995)
+        #     else:
+        #         print("hold btc : {0}".format(btc))
+        #         print("hold won : {0}".format(krw))
 
         time.sleep(60)
     except Exception as e:
